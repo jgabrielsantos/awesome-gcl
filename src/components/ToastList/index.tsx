c7720b6288651a8c12a04cfc1505a56bd867f548
jgabrielsantos <48341861+carvalhojg@users.noreply.github.com>
2023-10-30 14:47:17 -0700

    toast list and toast components

--- src/components/ToastList/index.tsx
+++ src/components/ToastList/index.tsx
@@ -3,10 +3,51 @@ import { ToastListComponentPropType } from "./types";
 import { ToastListStyles } from "./styles";
 import { Toast } from "./Components";
 
+/**
+ * Toast List component
+ * @param {ToastListComponentPropType} props
+ * @property {toastComponentPropTypes} props.toastList
+ * @property {string[]} props.additionalClasses
+ * @example
+ * <ToastList
+ *  toastList={[
+ *    {
+ *      id: 'success toast',
+ *      isOpen: successToastOpen,
+ *      handleClose: () => setSuccessToastOpen(false),
+ *      useCase: 'success',
+ *      children: <p>Success</p>,
+ *    },
+ *    {
+ *      id: 'fail toast',
+ *      isOpen: failToastOpen,
+ *      handleClose: () => setFailToastOpen(false),
+ *      useCase: 'fail',
+ *      children: <p>Fail</p>,
+ *    },
+ *    {
+ *      id: 'warning toast',
+ *      isOpen: warningToastOpen,
+ *      handleClose: () => setWarningToastOpen(false),
+ *      useCase: 'warning',
+ *      children: <p>Warning</p>,
+ *    },
+ *    {
+ *      id: 'info toast',
+ *      isOpen: infoToastOpen,
+ *      handleClose: () => setInfoToastOpen(false),
+ *      useCase: 'info',
+ *      children: <p>Info</p>,
+ *    },
+ *  ]}
+ * />
+ * @returns {JSX.Element} ToastList
+ */
+
 export const ToastList = ({
   toastList,
   additionalClasses = []
-}: Readonly<ToastListComponentPropType>) => {
+}: Readonly<ToastListComponentPropType>): JSX.Element => {
   const style = new ToastListStyles(additionalClasses)
   const { wrapperClass } = style.buildStyleRules()
 
