da34e75a1bec4b41f8118e5d105770a40ff8a10a
jgabrielsantos <48341861+carvalhojg@users.noreply.github.com>
2023-10-30 14:57:15 -0700

    use component

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -2,6 +2,33 @@ import React from "react";
 import { UserPropTypes } from "./types";
 import { UserStyles } from "./styles";
 
+/**
+ * User component
+ * @param {UserPropTypes} props
+ * @property {string} [props.profileImage] - Url used as src for img HTML tag
+ * @property {string} props.firstName - Determines initials value in case no image is provided
+ * @property {string} props.lastName - Determines initials value in case no image is provided
+ * @property {string} [props.description] - Determines the value for description an if it should be rendered
+ * @property {GSizeEnum} props.size
+ * @property {UserAdditionalClasses} [props.additionalClasses] - Option css classes
+ * @property {string[]} [props.additionalClasses.wrapper] - CSS classes for wrapper div HTML tag
+ * @property {string[]} [props.additionalClasses.avatar] - CSS classes for img HTML tag
+ * @property {string[]} [props.additionalClasses.initials] - CSS classes for initials div HTML tag
+ * @property {string[]} [props.additionalClasses.info] - CSS classe for indo div HTML tag
+ * @property {string[]} [props.additionalClasses.name] = CSS classes for name p HTML tag
+ * @property {string[]} [props.additionalClasses.description] - CSS classes for description p HTML tag
+ * @example
+ * <User
+ *  size='large'
+ *  profileImage:'path/to/image'
+ *  firstName: 'John'
+ *  lastName: 'Doe'
+ *  description: 'johndoe@email.com'
+ * />
+ * 
+ * @returns {JSX.Element} User
+ */
+
 export const User = ({
   profileImage,
   firstName,
